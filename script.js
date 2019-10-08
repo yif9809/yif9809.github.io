e8f69956d2fcc69b11220f5fa4a613ef00550446
yif9809 <56266214+yif9809@users.noreply.github.com>
2019-10-08 11:19:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Yi F"
+let userName = "yif9809";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
